26c84f5c52742b9626770863acf059ed9831872f
semenodp <dmitry.p.semenov@vanderbilt.edu>
2019-01-21 23:23:49 -0600

    Structure Update

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,23 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cssnano = require('gulp-cssnano');
-const browserSync = require('browser-sync');
 const plumber = require('gulp-plumber');
 
-
-gulp.task('browser-sync', () => {
-    browserSync({
-        server: {
-            baseDir: 'dist'
-        },
-        notify: false
-    });
+gulp.task('scss', () => {
+    return gulp
+        .src('dev/scss/**/*.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(
+            autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+                cascade: true
+            })
+        )
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('default', gulp.series('browser-sync', () => {
 
-    gulp.watch('dist/*.html', browserSync.reload);
-}));
+gulp.task('default', ['scss'], () => {
+    gulp.watch('dev/scss/**/*.scss', ['scss']);
+});
