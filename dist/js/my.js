872e6c3cf148ceb3823413316d491bce35a7b3ec
Evgenii <parapheen6@gmail.com>
2019-02-06 14:27:02 +0300

    minor updates. remains the issue with .dropdown__content in mobile versions

--- dist/js/my.js
+++ dist/js/my.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
     $('.burger').click(function () {
         $('.menu').toggleClass('active')
     })
-})
+});
 $(document).ready(function () {
     $('.login__icon').click(function(){
         $('.dropdown__login').toggleClass('active')
     })
-})
+});
