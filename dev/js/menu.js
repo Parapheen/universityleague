cbf210763e5d1f60f089fcbfda15226828e227c7
semenodp <39551930+semenodp@users.noreply.github.com>
2019-02-06 16:44:19 -0600

    Menu Update 2

--- dev/js/menu.js
+++ dev/js/menu.js
@@ -1,10 +1,27 @@
-$(document).ready(function () {
-    $('.burger').click(function () {
-        $('.menu').toggleClass('active')
-    })
-});
-$(document).ready(function () {
-    $('.login__icon').click(function(){
-        $('.dropdown__login').toggleClass('active')
+// $(document).ready(function () {
+//     $('.burger').click(function () {
+//         $('.menu').toggleClass('active')
+//     })
+// });
+// $(document).ready(function () {
+//     $('.login__icon').click(function(){
+//         $('.dropdown__login').toggleClass('active')
+//     })
+// });
+
+$(function() {
+    // toggle
+    var flag = true;
+    $('.login__icon').on('click', function (e) {
+        e.preventDefault();
+        if (flag) {
+            flag = false;
+            $('.dropdown__login').show('slow');
+            $('.burger').hide();
+        } else {
+            flag = true;
+            $('.burger').show('slow');
+            $('.dropdown__login').hide();
+        }
     })
 });
